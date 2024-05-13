1f8d0bbc3bba4ea6247120ee7f9fecdeea543663
Samady Correa <samadybiasoli@gmail.com>
2024-05-12 22:39:26 -0300

    Changed some stuff

--- ExercicioModulo9/Projeto/src/js/custom.js
+++ ExercicioModulo9/Projeto/src/js/custom.js
@@ -41,25 +41,6 @@ $(document).ready(function () {
 
   verificaEstoque();
 
-  // $('.featured-item:first h4').start('<span class="badge bg-secondary">Novo</span>')
-  // $('.featured-item:first h4').html('<span class="badge bg-secondary">Novo</span>')
-  // $('.featured-item:first h4').addClass('active')
-  // $('.featured-item:first h4').removeClass('active')
-  // $('.featured-item:first h4').toggleClass('active')
-  // $('.featured-item:first h4').after() Add some html after the given element
-  // $('.featured-item:first h4').before() Add  some html before some given element
-  // $('.featured-item:first h4').hide() Hide the html adding the atributte display:none at the element
-  // $('.featured-item:first h4').show() Take out the display:none atributte
-  // $('.featured-item:first h4').remove() remove the element from the DOM
-  // $('.featured-item:first h4').fadeIn(2000) Add a transition to the element's apparance
-  // $('.featured-item:first h4').fadeOut() Add a transition to the element's desapparance
-  // $('.featured-item:first h4').css('color', '#f00')
-  /* $('.featured-item:first h4').css({
-                                          'color': '#f00',
-                                          'background': '#ff0',
-                                          'font-weight': '100'
-                                       }) */
-
   /* Manipulação de Html e Css*/
 
   $(".featured-item h4").dblclick(function () {
@@ -78,34 +59,7 @@ $(document).ready(function () {
     alert("Produto esgotado");
   });
 
-  /*
-   * Callback
-   * Entendendo ações que começam ao termino de outra
-   */
-/*
-  $(".featured-item:nth(1)")
-    .hide(500, function () {
-      console.log($(this).find("h4").text() + " esgotado");
-    })
-    .show(500, function () {
-      console.log($(this).find("h4").text() + " em estoque");
-    });
-*/
-  /*
-   * Animaçoes
-   *
-   */
-/*
-  const time = 1000;
 
-  $(".featured-item:nth(0)")
-    .hide(time)
-    .show(time)
-    .fadeOut(time)
-    .fadeIn(time)
-    .toggle(time)
-    .toggle(time);
-*/
   $("#form-submit").on("click", function (event) {
     event.preventDefault();
     if ($("#email").val() != "") {
@@ -164,14 +118,6 @@ $(document).ready(function () {
       $(this).submit();
     }
   });
-
-  /*
-   * Funcao de Validacao dos Campos do Formulario
-   * TODO:
-   * 1- Incrementar se o nome é valido (mais de 2 caracteres)
-   * 2- Checar se o email é valido (necessita do formato padrao de email /[a-z]@[a-z]\.com/)
-   * 3- Checar a validade da entrada do campo do CPF
-   */
 
   function valFormFields(element) {
     if (element.val() == "") {
