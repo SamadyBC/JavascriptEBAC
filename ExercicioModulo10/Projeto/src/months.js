c692ad9b0a6cb5f58d4430df0f7b938d1a8676aa
Samady Correa <samadybiasoli@gmail.com>
2024-05-26 00:32:57 -0300

    Added some files

---
+++ ExercicioModulo10/Projeto/src/months.js
@@ -0,0 +1,57 @@
+let months = [
+    {   
+        month: 'Janeiro',
+        dias: 31
+    },
+    {
+        month: 'Fevereiro',
+        dias: 28
+    },
+    {
+        month: 'Março',
+        dias: 31
+    },
+    {
+        month: 'Abril',
+        dias: 30
+    },
+    {
+        month: 'Maio',
+        dias: 31
+    },
+    {
+        month: 'Junho',
+        dias: 30
+    },
+    {
+        month: 'Julho',
+        dias: 31
+    },
+    {
+        month: 'Agosto',
+        dias: 31
+    },
+    {
+        month: 'Setembro',
+        dias: 30
+    },
+    {
+        month: 'Outubro',
+        dias: 31
+    },
+    {
+        month: 'Novembro',
+        dias: 30
+    },
+    {
+        month: 'Dezembro',
+        dias: 31
+    }
+]
+
+function getMonths(){
+    return months;
+}
+
+
+export default getMonths();
