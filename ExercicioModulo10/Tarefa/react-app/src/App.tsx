f05f7699703dfa77d4b313fac19e11742204d85a
Samady Correa <samadybiasoli@gmail.com>
2024-08-06 23:07:50 -0300

    Added the task folder

---
+++ ExercicioModulo10/Tarefa/react-app/src/App.tsx
@@ -0,0 +1,8 @@
+
+import ListGroup from './components/ListGroup';
+
+function App() {
+  return <div><ListGroup/></div> // Podemos usar os modulos react como html tags - nesse caso estamos usando o auto fechamento na sintaxe de fechamento 
+}
+
+export default App;
---
+++ ExercicioModulo10/Tarefa/react-app/src/Message.tsx
@@ -0,0 +1,12 @@
+// PascalCasing - react expected notation
+function  Message(){
+    // JSX: Javascript SML
+    const name = "Samady"
+    if (name){
+        return <h1> Hello {name}</h1>;   
+    } else {
+        return <h1> Hello Barchik</h1>;
+    }
+}
+
+export default Message;
