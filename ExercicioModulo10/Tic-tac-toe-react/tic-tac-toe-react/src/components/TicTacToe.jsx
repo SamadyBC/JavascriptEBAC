e116a9ddf6d1a1fec9d4dc2d2cf8c9b8029e3a82
Samady Correa <samadybiasoli@gmail.com>
2024-08-13 15:50:35 -0300

    Finished the game

--- ExercicioModulo10/Tic-tac-toe-react/tic-tac-toe-react/src/components/TicTacToe.jsx
+++ ExercicioModulo10/Tic-tac-toe-react/tic-tac-toe-react/src/components/TicTacToe.jsx
@@ -1,25 +1,34 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Board from "./Board";
-import { useEffect } from "react";
+import GameOver from "./GameOver";
+import GameState from "./GameState";
+import Reset from "./Reset";
+import gameOverSoundAsset from "../sounds/game_over.wav";
+import clickSoundAsset from "../sounds/click.wav";
+
+const gameOverSound = new Audio(gameOverSoundAsset);
+gameOverSound.volume = 0.2;
+const clickSound = new Audio(clickSoundAsset);
+clickSound.volume = 0.5;
 
 const PLAYER_X = "X";
 const PLAYER_O = "O";
 
 const winningCombinations = [
   // Rows
-  { combo: [0, 1, 2], strike: "strike-row-1" },
-  { combo: [3, 4, 5], strike: "strike-row-2" },
-  { combo: [6, 7, 8], strike: "strike-row-3" },
+  { combo: [0, 1, 2], strikeClass: "strike-row-1" },
+  { combo: [3, 4, 5], strikeClass: "strike-row-2" },
+  { combo: [6, 7, 8], strikeClass: "strike-row-3" },
   // Columns
-  { combo: [0, 3, 6], strike: "strike-column-1" },
-  { combo: [1, 4, 7], strike: "strike-column-2" },
-  { combo: [2, 5, 8], strike: "strike-column-3" },
+  { combo: [0, 3, 6], strikeClass: "strike-column-1" },
+  { combo: [1, 4, 7], strikeClass: "strike-column-2" },
+  { combo: [2, 5, 8], strikeClass: "strike-column-3" },
   // Diagonal
-  { combo: [0, 4, 8], strike: "strike-diagonal-1" },
-  { combo: [2, 4, 6], strike: "strike-diagonal-2" },
+  { combo: [0, 4, 8], strikeClass: "strike-diagonal-1" },
+  { combo: [2, 4, 6], strikeClass: "strike-diagonal-2" },
 ];
 
-function checkWinner(tiles, setStrikeClass) {
+function checkWinner(tiles, setStrikeClass, setGameState) {
   for (const { combo, strikeClass } of winningCombinations) {
     const tileValue1 = tiles[combo[0]];
     const tileValue2 = tiles[combo[1]];
@@ -31,32 +40,67 @@ function checkWinner(tiles, setStrikeClass) {
       tileValue1 === tileValue3
     ) {
       setStrikeClass(strikeClass);
+      if (tileValue1 === PLAYER_X) {
+        setGameState(GameState.playerXWins);
+      } else {
+        setGameState(GameState.playerOWins);
+      }
+      return;
     }
+  }
+
+  const areAllTilesFilledIn = tiles.every((tile) => tile !== null);
+  if (areAllTilesFilledIn) {
+    setGameState(GameState.draw);
   }
 }
 
 function TicTacToe() {
-  const [tiles, SetTiles] = useState(Array(9).fill(null));
-  const [playerTurn, SetPlayerTurn] = useState(PLAYER_X);
+  const [tiles, setTiles] = useState(Array(9).fill(null));
+  const [playerTurn, setPlayerTurn] = useState(PLAYER_X);
   const [strikeClass, setStrikeClass] = useState();
+  const [gameState, setGameState] = useState(GameState.inProgress);
 
   const handleTileClick = (index) => {
+    if (gameState !== GameState.inProgress) {
+      return;
+    }
+
     if (tiles[index] !== null) {
       return;
     }
     const newTiles = [...tiles]; // O que significa essa sintaxe?
     newTiles[index] = playerTurn;
-    SetTiles(newTiles);
+    setTiles(newTiles);
     if (playerTurn === PLAYER_X) {
-      SetPlayerTurn(PLAYER_O);
+      setPlayerTurn(PLAYER_O);
     } else {
-      SetPlayerTurn(PLAYER_X);
+      setPlayerTurn(PLAYER_X);
     }
   };
 
+  const handleReset = () => {
+    setGameState(GameState.inProgress);
+    setTiles(Array(9).fill(null));
+    setPlayerTurn(PLAYER_X);
+    setStrikeClass(null);
+  };
+
   useEffect(() => {
-    checkWinner(tiles, setStrikeClass);
+    checkWinner(tiles, setStrikeClass, setGameState);
   }, [tiles]);
+
+  useEffect(() => {
+    if (tiles.some((tile) => tile !== null)) {
+      clickSound.play();
+    }
+  }, [tiles]);
+
+  useEffect(() => {
+    if (gameState !== GameState.inProgress) {
+      gameOverSound.play();
+    }
+  }, [gameState]);
 
   return (
     <div>
@@ -67,6 +111,8 @@ function TicTacToe() {
         onTileClick={handleTileClick}
         strikeClass={strikeClass}
       />
+      <GameOver gameState={gameState} />
+      <Reset gameState={gameState} onReset={handleReset} />
     </div>
   );
 }
