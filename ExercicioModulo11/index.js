5aaec1812ffba42d9d199a2f0e991197425930ee
Samady Correa <samadybiasoli@gmail.com>
2024-08-15 22:35:33 -0300

    Add the REST api methods

--- ExercicioModulo11/index.js
+++ ExercicioModulo11/index.js
@@ -1,4 +1,4 @@
-const http = require("http");
+/*const http = require("http");
 
 const host = "127.0.0.1";
 const port = 3000;
@@ -11,4 +11,40 @@ const server = http.createServer((req, res) => {
 
 server.listen(port, host, () => {
   console.log(`Servidor rodando em http://${host}:${port}/`); // Backtick for derefering
+});
+*/
+
+const express = require("express");
+const server = express();
+
+server.listen(3001);
+
+server.use(express.json());
+const contacts = [
+  {
+    name: "samady",
+  },
+];
+
+server.get("/", () => {
+  console.log("executando a rota / com o GET e nodemon");
+});
+
+server.get("/contact", (req, res) => {
+  return res.json(contacts);
+});
+
+// Adiciona a lista
+server.post("/contact", (req, res) => {
+  const { name } = req.body;
+  contacts.push(req.body);
+  return res.json(contacts);
+});
+
+server.put("/contact/:index", (req, res) => {
+  const { index } = req.params;
+  const { name } = req.body;
+
+  contacts[index] = name;
+  return res.json(contacts);
 });
