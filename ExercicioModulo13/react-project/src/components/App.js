09a46725a8e41430ea8752dfefc26fb1f6501301
Samady Correa <samadybiasoli@gmail.com>
2024-08-26 14:01:56 -0300

    Styled the buttons

--- ExercicioModulo13/react-project/src/components/App.js
+++ ExercicioModulo13/react-project/src/components/App.js
@@ -1,16 +1,22 @@
 import "./App.css";
-import Button from "./Button";
+//import Button from "./Button";
 import Counter from "./Counter";
 import CounterState from "./Counter2";
+import { Title, Btn, BtnNew } from "./styles";
 
 function App() {
   return (
     <div>
+      <Title>
+        Styled Components
+        <span> com react</span>
+      </Title>
       <Counter />
       <hr />
       <CounterState />
       <hr />
-      <Button name="React Button" />
+      <Btn name="React Button" font="50" />
+      <BtnNew name="React Button" font="50" />
     </div>
   );
 }
